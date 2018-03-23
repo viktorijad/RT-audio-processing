2b94487fe81ed4cc3876b034162f7bb1d252af95
Viktorija Dimova <viktorija.oh@gmail.com>
2018-03-23 13:31:44 +0100

    added part for LED strip

--- rpi_script.py
+++ rpi_script.py
@@ -16,14 +16,103 @@ import librosa
 import librosa.display
 import parselmouth
 import seaborn
+import time
+from neopixel import *
+import argparse
+import RPi.GPIO as GPIO
 seaborn.set()
 
 ms.use('seaborn-muted')
 
 #style.use('fivethirtyeight')
 
+# LED strip configuration:
+LED_COUNT      = 23      # Number of LED pixels.
+LED_PIN        = 18      # GPIO pin connected to the pixels (18 uses PWM!).
+#LED_PIN        = 10      # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
+LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
+LED_DMA        = 10      # DMA channel to use for generating signal (try 10)
+LED_BRIGHTNESS = 150     # Set to 0 for darkest and 255 for brightest
+LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
+LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
+
+COL_RED         = Color(0, 255, 0)
+COL_GREEN       = Color(255, 0, 0)
+COL_YELLOW      = Color(255, 255, 0)
+
+# Define in and output pins
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(25, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+# Define functions which animate LEDs in various ways.
+# def colorWipe(strip, color, wait_ms=50):
+    # """Wipe color across display a pixel at a time."""
+    # for i in range(strip.numPixels()):
+        # strip.setPixelColor(i, color)
+        # strip.show()
+        # time.sleep(wait_ms/1000.0)
+
+# def theaterChase(strip, color, wait_ms=50, iterations=10):
+    # """Movie theater light style chaser animation."""
+    # for j in range(iterations):
+        # for q in range(3):
+            # for i in range(0, strip.numPixels(), 3):
+                # strip.setPixelColor(i+q, color)
+            # strip.show()
+            # time.sleep(wait_ms/1000.0)
+            # for i in range(0, strip.numPixels(), 3):
+                # strip.setPixelColor(i+q, 0)
+
+def wheel(pos):
+    """Generate rainbow colors across 0-255 positions."""
+    if pos < 85:
+        return Color(pos * 3, 255 - pos * 3, 0)
+    elif pos < 170:
+        pos -= 85
+        return Color(255 - pos * 3, 0, pos * 3)
+    else:
+        pos -= 170
+        return Color(0, pos * 3, 255 - pos * 3)
+
+# def rainbow(strip, wait_ms=20, iterations=1):
+    # """Draw rainbow that fades across all pixels at once."""
+    # for j in range(256*iterations):
+        # for i in range(strip.numPixels()):
+            # strip.setPixelColor(i, wheel((i+j) & 255))
+        # strip.show()
+        # time.sleep(wait_ms/1000.0)
+
+# def rainbowCycle(strip, wait_ms=20, iterations=5):
+    # """Draw rainbow that uniformly distributes itself across all pixels."""
+    # for j in range(256*iterations):
+        # for i in range(strip.numPixels()):
+            # strip.setPixelColor(i, wheel((int(i * 256 / strip.numPixels()) + j) & 255))
+        # strip.show()
+        # time.sleep(wait_ms/1000.0)
+
+# def theaterChaseRainbow(strip, wait_ms=50):
+    # """Rainbow movie theater light style chaser animation."""
+    # for j in range(256):
+        # for q in range(3):
+            # for i in range(0, strip.numPixels(), 3):
+                # strip.setPixelColor(i+q, wheel((i+j) % 255))
+            # strip.show()
+            # time.sleep(wait_ms/1000.0)
+            # for i in range(0, strip.numPixels(), 3):
+                # strip.setPixelColor(i+q, 0)
+
+def simpleColor(strip, color):
+    """ Just show a single color."""
+    strip.setPixelColor(12, color)
+    strip.show()
+    # time.sleep(0.5)
+    # strip.setPixelColor(12, Color(0,0,0))
+    # strip.show()
+    # time.sleep(0.5)
+
 CHANNELS = 1
-RATE = 22050 # changed from 44100, Viktorija
+SAMPLING_RATE = 22050 # Viktorija changed from 44100
 
 p = pyaudio.PyAudio()
 fulldata = np.array([])
@@ -76,30 +165,31 @@ def draw_intensity(intensity):
     plt.ylim(0)
     plt.ylabel("intensity [dB]")
 
-def analyze():
-	audio_path = 'C://Users/Fabi/Documents/presentation-analysis/audio/test_1.wav'
-	y, sr = librosa.load(audio_path, dtype=np.float32)
-	snd = parselmouth.Sound("./audio/nash_1.wav")
-	intensity = snd.to_intensity()
-	print(len(intensity.xs()))
-	spectrogram = snd.to_spectrogram()
-	print(len(spectrogram.x_grid()))
-	plt.figure()
-	draw_spectrogram(spectrogram)
-	plt.twinx()
-	draw_intensity(intensity)
-	plt.xlim([snd.xmin, snd.xmax])
-	plt.show() # or plt.savefig("spectrogram.pdf")
-	audio_path = './audio/monoton.wav'
+def analyze(snd):
+	# audio_path = 'C://Users/Fabi/Documents/presentation-analysis/audio/test_1.wav'
+	# y, sr = librosa.load(audio_path, dtype=np.float32)
+	# snd = parselmouth.Sound("./audio/nash_1.wav")
+	# intensity = snd.to_intensity()
+	# print(len(intensity.xs()))
+	# spectrogram = snd.to_spectrogram()
+	# print(len(spectrogram.x_grid()))
+	# plt.figure()
+	# draw_spectrogram(spectrogram)
+	# plt.twinx()
+	# draw_intensity(intensity)
+	# plt.xlim([snd.xmin, snd.xmax])
+	# plt.show() # or plt.savefig("spectrogram.pdf")
+	# audio_path = './audio/monoton.wav'
 
-	y, sampling_rate = librosa.load(audio_path, dtype=np.float32)
-	print(len(y))
+	# y, sampling_rate = librosa.load(audio_path, dtype=np.float32)
+	# print(len(y))
 
 	segmentation_time = 0.1
 
 	# Time of the Signal
-	sr = int(sampling_rate * segmentation_time)
-	NoWindows = int(np.ceil(y.size/sr))
+	sr = int(SAMPLING_RATE * segmentation_time)
+	# NoWindows = int(np.ceil(y.size/sr))
+	NoWindows = 100 # todo: discuss which value should this have
 	signal_avg = np.zeros(NoWindows)
 	signal_max = np.zeros(NoWindows)
 	signal_min = np.zeros(NoWindows)
@@ -107,8 +197,8 @@ def analyze():
 	intensitys = np.zeros(NoWindows)
 	std = np.zeros((NoWindows,sr))
 
-	snd = parselmouth.Sound(audio_path)
-	intensity = snd.to_intensity(time_step=0.1)
+	# snd = parselmouth.Sound(audio_path)
+	intensity = snd.to_intensity(time_step=0.1) # todo: check if this works
 	pitch = snd.to_pitch(time_step=0.1)
 
 	print(len(intensity.values[0]))
@@ -210,15 +300,63 @@ def animate(i):
     buffer = buffer + audio_data
 
 def main():
+######################LED STRIPS START#########################
+    # Process arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-c', '--clear', action='store_true', help='clear the display on exit')
+    args = parser.parse_args()
+
+    # Create NeoPixel object with appropriate configuration.
+    strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
+    # Intialize the library (must be called once before other functions).
+    strip.begin()
+	
+	simpleColor(strip, COL_GREEN)
+
+    # print ('Press Ctrl-C to quit.')
+    # if not args.clear:
+        # print('Use "-c" argument to clear LEDs on exit')
+
+    # try:
+
+        # while True:
+            # """
+            # print ('Color wipe animations.')
+            # colorWipe(strip, Color(255, 0, 0))  # Red wipe
+            # colorWipe(strip, Color(0, 255, 0))  # Blue wipe
+            # colorWipe(strip, Color(0, 0, 255))  # Green wipe
+            # print ('Theater chase animations.')
+            # theaterChase(strip, Color(127, 127, 127))  # White theater chase
+            # theaterChase(strip, Color(127,   0,   0))  # Red theater chase
+            # theaterChase(strip, Color(  0,   0, 127))  # Blue theater chase
+            # print ('Rainbow animations.')
+            # rainbow(strip)
+            # rainbowCycle(strip)
+            # theaterChaseRainbow(strip)
+            # """
+            # if GPIO.input(24) == 0:
+                # simpleColor(strip, COL_YELLOW)
+            # elif GPIO.input(25) == 0:
+                # simpleColor(strip, COL_RED)
+            # else:
+                # simpleColor(strip, Color(0,0,0))
+
+    # except KeyboardInterrupt:
+        # if args.clear:
+            # colorWipe(strip, Color(0,0,0), 10)
+######################LED STRIPS STOP##########################
     stream = p.open(format=pyaudio.paFloat32,
                 channels=CHANNELS,
-                rate=RATE,
+                rate=SAMPLING_RATE,
                 output=False,
                 input=True,
                 stream_callback=callback)
     stream.start_stream()
     ani = animation.FuncAnimation(fig, animate, interval=100)
     plt.show() # what is this doing?
+	analyze(ani) # todo: this might not work, check items
+	# todo: call the simpleColor(strip, color) function, set the color 
+	#		according to the analysis of the speech
     while stream.is_active():
         time.sleep(10)
         stream.stop_stream()
